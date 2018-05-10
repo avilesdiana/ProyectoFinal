d1e0ab7f3dd6d96c0d9a080ddee00fffc68651ff
avilesdiana <avilesdiana@outlook.com>
2018-05-10 00:34:27 -0500

    Direccionamiento por Patricio Cano

--- funcionDireccionamiento.c
+++ funcionDireccionamiento.c
@@ -1,15 +1,15 @@
 #include "funciones.h"
 
 /**
- * 
+ *
  * @author [Diego Patricio Gutierrez]
  * @date [5-mayo-2018]
  * @brief [Funcion que imprime el direccionamiento]
  * @file [muestraRuta.c]
- * 
+ *
  * @nodoEstructura *nodo [Recibo la estructura del nodo]
  * @i[nodo inicial]
- * 
+ *
  * */
 void funcionDireccionamiento (nodoEstructura *nodo, int i) {
 
@@ -25,17 +25,17 @@ void funcionDireccionamiento (nodoEstructura *nodo, int i) {
     printf("No se encuentra ruta....\n");
     return;
   }
-    
+
   for (auxVerticeE = auxVerti; auxVerticeE->distancia; auxVerticeE = nodo->vertices[auxVerticeE->nodoPrevio], n++) {
   }
-    
+
   ruta[n - 1] = 'a' + i;
   printf("La ruta es: \n\n");
   for (auxVerticeE = auxVerti; auxVerticeE->distancia; auxVerticeE = nodo->vertices[auxVerticeE->nodoPrevio], j++) {
-        
+
     ruta[n - j - 2] = 'a' + auxVerticeE->nodoPrevio;
     printf("Ruta: %c con costo de: %d\n", ruta[n - j - 2], auxVerticeE->nodoPrevio);
-  } 
+  }
 
   printf("%d <-%.*s\n", auxVerti->distancia, n, ruta);
 
